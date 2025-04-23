62c03fc985413bf6939cbebedd1be52bbdda3aed
L1nMay <ilazayzev@gmail.com>
2025-04-23 08:03:14 -0400

    New test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,24 +1,14 @@
-#include <gtest/gtest.h>
-#include <fstream>
-#include "print.hpp"
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+#include "funcs.h"
 
-// Пример функции, которую мы будем тестировать
-int add(int a, int b) {
-  return a + b;
-}
-
-// Тест, который должен пройти
 TEST(AddTest, PositiveNumbers) {
-  ASSERT_EQ(add(2, 2), 4);
+    ASSERT_EQ(add(2, 2), 4);
 }
 
-// Тест, который мы намеренно сломаем
 TEST(AddTest, NegativeNumbers) {
-  ASSERT_EQ(add(-2, -2), -5);  // <--- Намеренная ошибка: должно быть -4
+    ASSERT_EQ(add(-2, -2), -4);  // Исправлено: ожидаем -4, а не -5
 }
 
-// Еще один тест, который должен пройти
 TEST(AddTest, Zero) {
-  ASSERT_EQ(add(0, 0), 0);
+    ASSERT_EQ(add(0, 0), 0);
 }
