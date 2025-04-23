83803d2f37cbd879f5409e90fa1271df7c9bb3d3
L1nMay <ilazayzev@gmail.com>
2025-04-23 07:59:14 -0400

    Remove _build directory

--- _build/_build/third-party/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kali/workspace/lab05/third-party/gtest
-# Build directory: /home/kali/workspace/lab05/_build/_build/third-party/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../../_build/third-party/gtest/googlemock")
